15b73e887c3e5fe082fd990175b51393dc4be82d
Khaled-The-Dev <116115522+Khaled-The-Dev@users.noreply.github.com>
2022-12-22 15:12:53 +0100

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,10 +1,2 @@
-export const Config = {
-   Keys: {
-     Supabase:' eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpeXp6YmV5amF4Z25nZXV1bnNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEwMzEzNTQsImV4cCI6MTk4NjYwNzM1NH0.YXZL-PjiPWKVJI20MeX6UD411sOmAKFNh9ubmwgk9dE'
-   },
-   
-   Urls: {
-     Supabase: 'https://iiyzzbeyjaxgngeuunsa.supabase.co'
-   }
-}
+// api keys here
 
